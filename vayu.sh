2fc0b48011494025abbac2775050122b43b0429f
grepfox <grepfox@tutamail.com>
2025-12-31 21:46:43 +0530

    Setup build environment and clone necessary repositories
    Initialize repository, clone device-specific files, sync ROM, and set up build environment.

--- vayu.sh
+++ vayu.sh
@@ -1,3 +1,21 @@
+# init repo
+repo init -u https://github.com/yaap/manifest.git -b sixteen --git-lfs --depth=1
+
+# clone device stuff
+git clone https://github.com/grepfox/local_manifest -b yaap .repo/local_manifests
+
+# sync rom
+repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
+
+# clone and displayfeatures
+rm -rf packages/apps/DisplayFeatures
+git clone https://github.com/yaap/packages_apps_DisplayFeatures packages/apps/DisplayFeatures
+
+# Signing keys
+rm -rf vendor/yaap/signing/keys
+git clone https://github.com/grepfox/yaap_sign vendor/yaap/signing/keys
+
+# setup build env
 source build/envsetup.sh
 
 # timezone
